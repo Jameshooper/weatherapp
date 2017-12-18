4a6eb033167facb9e882d7a470ee56758d9bb72a
Jameshooper <Jameshooper@users.noreply.github.com>
2017-12-18 12:50:35 +0200

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || '88510c0ca4c7d0e4f0ade76c22e6c641';
+const appId = process.env.APPID || '';
 const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
 const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
 
